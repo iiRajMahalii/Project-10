599b8b3256c2b5eaa5d9d3171bca405e5ee7c246
Suraj <Eagle77@Surajs-MacBook-Pro.local>
2014-04-10 15:25:20 -0400

    Initial Commit

---
+++ Project 10/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Project 10
+//
+//  Created by Suraj on 4/10/14.
+//  Copyright (c) 2014 Suraj. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
